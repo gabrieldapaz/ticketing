e5f2dab85a6fd17f3de0ecbdb35b7d181de8c81a
gabrieldapaz <mgpp@cin.ufpe.br>
2020-06-05 23:52:17 -0300

    add serializeError method on error classes

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../erros/request-validation-error';
-import { DatabaseConnectionError } from '../erros/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
   err: Error,
@@ -9,16 +9,11 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    // Format all error messagem to one pattern.
-    const formattedErrors = err.errors.map((error) => {
-      return { message: error.msg, field: error.param };
-    });
-    // Using return to not execute the rest of the code
-    return res.status(400).send({ errors: formattedErrors });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
