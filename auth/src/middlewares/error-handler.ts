bc5af13eb8f49b65c2c1c26406cb983baec4fa85
gabrieldapaz <mgpp@cin.ufpe.br>
2020-06-05 23:36:18 -0300

    handle different error types

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -8,7 +8,20 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  if (err instanceof RequestValidationError) {
+    // Format all error messagem to one pattern.
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    // Using return to not execute the rest of the code
+    return res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: 'Something went wrong' }],
   });
 };
