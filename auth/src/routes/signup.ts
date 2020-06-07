93d779d5c4e3037e696456e5e34783c41fc6175e
gabrieldapaz <mgpp@cin.ufpe.br>
2020-06-07 20:27:16 -0300

    create middleware for sigin, create middleware for validate request

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -17,15 +17,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 charcters'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    // Append information about possibles erros on the request
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      // The string passed into Error will be attached to 'message' property
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -49,7 +42,7 @@ router.post(
     // Store it on session object
     // This session wil be sent to the browser
     req.session = {
-      jwt: userJwt
+      jwt: userJwt,
     };
 
     res.status(201).send(user);
