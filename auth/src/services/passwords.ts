1c5b5928248b80ec36936c606c3330f653b2eba7
gabrieldapaz <mgpp@cin.ufpe.br>
2020-06-06 22:09:49 -0300

    add password class

--- auth/src/services/passwords.ts
+++ auth/src/services/passwords.ts
@@ -4,3 +4,20 @@ import { promisify } from 'util';
 // Going from a callback implementation
 // to a promise base implementation
 const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString('hex');
+    // Array with raw data inside
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return `${buf.toString('hex')}.${salt}`;
+  }
+
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString('hex') === hashedPassword;
+  }
+}
