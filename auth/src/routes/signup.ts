10f7a62ea63f1f9141b8bf40972d5a8d9997b032
gabrieldapaz <mgpp@cin.ufpe.br>
2020-06-13 14:04:05 -0300

    update currentuser route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -42,7 +42,7 @@ router.post(
     // Store it on session object
     // This session wil be sent to the browser
     req.session = {
-      jwt: userJwt,
+      jwt: userJwt
     };
 
     res.status(201).send(user);
