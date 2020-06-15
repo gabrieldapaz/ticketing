58b3242b4bceaa34bfbc0149a94189605653a23f
gabrieldapaz <mgpp@cin.ufpe.br>
2020-06-15 19:53:04 -0300

    create app.ts file to setup express application

---
+++ auth/src/app.ts
@@ -0,0 +1,40 @@
+// app.ts just setup the express application
+
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+// Ensure that express is aware that is behind a proxy that's nginx
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // Don't encrypt
+    secure: true, // HTTPS
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// If didn't have the express-async-errors this wouldn't send the request
+// because would need the next
+// Make sure that express waits or essentially does in await statement on this function
+app.get('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,8 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
+// Start the express application
+
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-// Ensure that express is aware that is behind a proxy that's nginx
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, // Don't encrypt
-    secure: true, // HTTPS
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// If didn't have the express-async-errors this wouldn't send the request
-// because would need the next
-// Make sure that express waits or essentially does in await statement on this function
-app.get('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   // This function will wait for the connection, if don't connect, will throw an error
