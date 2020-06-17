9956ec6b63230f441efe09da159fddd4631a232d
gabrieldapaz <mgpp@cin.ufpe.br>
2020-06-17 20:16:46 -0300

    create a react component signup

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (event) => {
+    // Prevent the form submit itself to the browser
+    event.preventDefault();
+
+    console.log(email, password);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit}>
+        <h1>Sign Up</h1>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Sign Up</button>
+      </form>
+    </div>
+  );
+};
