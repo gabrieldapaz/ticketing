ae499acc7a8c6d37bf1c4202ef97f38f7bf2cc06
gabrieldapaz <mgpp@cin.ufpe.br>
2020-07-06 19:17:00 -0300

    update common library, fix show.test.ts errors (mongoose id, wrong route)

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,11 +1,11 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 404 if the ticket is not found', async () => {
-  const response = await request(app)
-    .get('/api/tickets/asdasdjkashdjsak')
-    .send()
-    .expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it('returns the ticket if the ticket is found', async () => {
@@ -13,9 +13,12 @@ it('returns the ticket if the ticket is found', async () => {
   const price = 20;
 
   const response = await request(app)
-    .post('/api/tickets/new')
+    .post('/api/tickets')
     .set('Cookie', global.signin())
-    .send({ title, price })
+    .send({
+      title,
+      price,
+    })
     .expect(201);
 
   const ticketResponse = await request(app)
