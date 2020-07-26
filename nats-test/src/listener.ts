6131cc298dbc52715a92d78cf1991d7d8dd61fe8
gabrieldapaz <mgpp@cin.ufpe.br>
2020-07-26 13:59:20 -0300

    add an event handler to ensure that when a listener goes down, NATS will exit and avoid copies, add ack options on subscription, solve issues of missing messages with signal process

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,9 +10,16 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true); // NATS won't say that recived some event. It's our task to do this and avoid loss the event with erros
   const subscription = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group '
+    'orders-service-queue-group',
+    options
   );
 
   subscription.on('message', (msg: Message) => {
@@ -21,5 +28,16 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+
+// Solve issue of missing messages
+// Signal interrupt
+process.on('SIGINT', () => stan.close());
+// Signal terminate
+process.on('SIGTERM', () => stan.close());
+
+// This doesn't cover all type of signals like SIGKILL
