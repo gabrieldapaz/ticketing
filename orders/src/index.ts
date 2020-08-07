7d7b47b7ddf825288eda99c367066ce2cdb028db
gabrieldapaz <mgpp@cin.ufpe.br>
2020-08-07 16:47:17 -0300

    Order: setup index for listen for ticket created and updated events

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,9 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
+import { Ticket } from './models/ticket';
 
 const start = async () => {
   // This function will wait for the connection, if don't connect, will throw an error
@@ -42,6 +45,9 @@ const start = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
