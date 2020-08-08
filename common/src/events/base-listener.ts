24b1be21a544d461267e475d4ce81ae32e0c9811
gabrieldapaz <mgpp@cin.ufpe.br>
2020-08-08 14:32:23 -0300

    m

--- common/src/events/base-listener.ts
+++ common/src/events/base-listener.ts
@@ -12,7 +12,7 @@ export abstract class Listener<T extends Event> {
   abstract subject: T['subject']; // When you use abstract properties you need to define on child classes
   abstract queueGroupName: string;
   abstract onMessage(data: T['data'], msg: Message): void;
-  private client: Stan;
+  protected client: Stan;
   protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
