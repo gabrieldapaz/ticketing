62235a1d4c467814f64d99ef5f3d2f3dd83b11c0
gabrieldapaz <mgpp@cin.ufpe.br>
2020-08-12 09:43:24 -0300

    Expiration: install packages, update index file

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,23 +1,12 @@
 // Start the express application
 
-import mongoose from 'mongoose';
-import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import { OrderCreatedListener } from './events/listeners/order-created-listener';
-import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
   // This function will wait for the connection, if don't connect, will throw an error
   // Instead of using localhost, we use the name of the clusterIP to access mongo db Pod
 
   // checking the enviroment variables to TypeScript not complaint
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY mus be defined');
-  }
-
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
 
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
@@ -44,23 +33,9 @@ const start = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
-
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    console.log('Connected to MongoDB');
   } catch (err) {
     console.log(err);
   }
-
-  app.listen(3000, () => {
-    console.log('Listening on port 3000');
-  });
 };
 
 start();
