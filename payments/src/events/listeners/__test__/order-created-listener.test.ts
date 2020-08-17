24ea2b28138726830222784a821496a3d9dda348
gabrieldapaz <mgpp@cin.ufpe.br>
2020-08-17 08:54:49 -0300

    Payments: create tests for order created and cancelled listeners, wrap up the listeners on index file

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,46 @@
+import { OrderCreatedListener } from '../order-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedEvent, OrderStatus } from '@dpztickets/common';
+import mongoose, { version } from 'mongoose';
+import { Order } from '../../../models/order';
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const data: OrderCreatedEvent['data'] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    expiresAt: 'asdasds',
+    userId: 'qasdkas',
+    status: OrderStatus.Created,
+    ticket: {
+      id: 'sadasd',
+      price: 10,
+    },
+  };
+
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it('replicates the order info', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order!.price).toEqual(data.ticket.price);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
--- payments/src/index.ts
+++ payments/src/index.ts
@@ -3,6 +3,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
   // This function will wait for the connection, if don't connect, will throw an error
@@ -42,6 +44,9 @@ const start = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
