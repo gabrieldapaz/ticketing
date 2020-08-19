a6c68eb05af6e1fd0c9ee2b0305e6af3b2aae7a7
gabrieldapaz <mgpp@cin.ufpe.br>
2020-08-19 09:21:45 -0300

    Client: list all tickets on landing page, fetch data from api

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,11 +3,29 @@
 // we don't get any opportunity to make request all of our components are executed
 // or rendered just one single time.
 
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1> You are signed in </h1>
-  ) : (
-    <h1> You are NOT signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
@@ -20,7 +38,9 @@ const LandingPage = ({ currentUser }) => {
 //declared in __app.js
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
 };
 
 export default LandingPage;
